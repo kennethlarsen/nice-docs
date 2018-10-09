f3b603620c9bcb6b730f17035deb0748cb6cf444
Kenneth Larsen <kla@linkfire.com>
2018-10-09 15:29:22 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const chalk       = require('chalk');
 const clear       = require('clear');
 const figlet      = require('figlet');
-const files = require('./lib/files');
 const inquirer  = require('./lib/inquirer');
-const bootstrap = require('./lib/bootstrap');
 const audit = require('./lib/audit');
 
 const run = async () => {
   const action = await inquirer.menu();
-  console.log(action);
-
-  if (action.menu === "Setup documentation") {
-    bootstrap.createDocSetup();
-  }
 
   if (action.menu === "Run audit") {
     audit.start();
@@ -22,7 +15,7 @@ const run = async () => {
 clear();
 console.log(
   chalk.yellow(
-    figlet.textSync('community docs', { horizontalLayout: 'full' })
+    figlet.textSync('nice docs', { horizontalLayout: 'full' })
   )
 );
 
